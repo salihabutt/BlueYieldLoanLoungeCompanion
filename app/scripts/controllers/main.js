846bf287bf0208acc6c6fdd749f5588ad3b2d453
Pablo Guevara <pablog.178@gmail.com>
2015-09-21 10:11:25 -0500

    Init dump

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blueYieldLoanLoungeCompanionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blueYieldLoanLoungeCompanionApp
+ */
+angular.module('blueYieldLoanLoungeCompanionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
