ea7562fc5d60c29debd5cc5c87a04375a4f2bc2d
Pablo Guevara <pablog.178@gmail.com>
2015-09-21 10:29:20 -0500

    Added ui-route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,29 @@
  * Main module of the application.
  */
 angular
-  .module('blueYieldLoanLoungeCompanionApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.module('blueYieldLoanLoungeCompanionApp', [
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'ui.router'
+	])
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('main');
+
+		$stateProvider
+			.state('main', {
+				url: '/',
+				templateUrl: 'views/main.html',
+				controller : 'MainCtrl',
+				controllerAs : 'main'
+			})
+			.state('about', {
+				url : '/about',
+				templateUrl: 'views/about.html',
+				controller: 'AboutCtrl',
+				controllerAs: 'about'
+			})
+	});
