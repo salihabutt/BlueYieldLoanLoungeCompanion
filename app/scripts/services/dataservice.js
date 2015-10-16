26b58a5d37b66b75ded7cf42dac99faea1005ce5
salihabutt <saliha.butt@tkxel.com>
2015-10-15 20:14:43 -0700

    BG-5485

--- app/scripts/services/dataservice.js
+++ app/scripts/services/dataservice.js
@@ -15,9 +15,9 @@ angular.module('blueYieldLoanLoungeCompanionApp')
 	    		address: "1234 Vita St, Paramount, CA 90723",
 	    		name2: "Steven Test",
 	    		address2: "1234 Vita St, Paramount, CA 90723",
-	    		cellphone: "(123)456-7890",
-	    		home: "(123)456-7890",
-	    		work: "(123)456-7890",
+	    		cellphone: "(123) 456-7890",
+	    		home: "(123) 456-7890",
+	    		work: "(123) 456-7890",
 	    		email: "test@gmail.com"
 	    	
 	    };
